a147bf4e03b2f676b9ab97e5457bdd929e67e0a2
Sasamus <labus93@gmail.com>
2015-02-21 11:48:11 +0100

    Created class Tester for testing purposes

---
+++ src/testing/Tester.java
@@ -0,0 +1,18 @@
+package testing;
+
+/**
+ * Class with main method used for testing/running the program
+ * 
+ * @author Albin Engström
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
