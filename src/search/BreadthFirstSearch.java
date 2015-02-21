55ed388d51859eed6a69fd7d1c942795df3506a5
Sasamus <labus93@gmail.com>
2015-02-21 12:59:24 +0100

    Created some of the variables needed for the search

--- src/search/BreadthFirstSearch.java
+++ src/search/BreadthFirstSearch.java
@@ -1,10 +1,40 @@
 package search;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+
 /**
  * Performs a Breadth First Search
  * 
  * @author Albin Engström
  */
 public class BreadthFirstSearch {
+
+	/**
+	 * Constructor
+	 */
+	public BreadthFirstSearch() {
+		
+	}
+	
+	public boolean Search(){
+		
+		// The node holding the state, currently the starting state
+		ArrayList<Integer> node = (ArrayList<Integer>) Arrays.asList(3, 3, 1);
+		
+		// The goal state
+		ArrayList<Integer> goal = (ArrayList<Integer>) Arrays.asList(0, 0, 0);
+		
+		// Holds the nodes of the frontier
+		ArrayDeque<ArrayList<Integer>> frontier = new ArrayDeque<ArrayList<Integer>>();
+		
+		// Holds the explored nodes
+		HashSet<ArrayList<Integer>> explored = new HashSet<ArrayList<Integer>>();
+		
+		return true;
+	}
 
 }
